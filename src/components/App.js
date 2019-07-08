4fe69a90a4f6e6633d44b16009dc64624eefc7b1
William Fleming <chaosmegaman@Williams-MacBook-Pro.local>
2019-07-08 14:49:09 -0400

    On the app component add icons, an onclick function to change text when flag is clicked, and added state

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React,{Component} from 'react'
 
-const App = () => {
-    return (
-        <div>App</div>
-    )
+class App extends Component {
+state = {
+    laguage: 'english'
+}
+
+onLanguageChange = (language) => {
+    this.setState({language : language})
+}
+
+    render(){
+     return (
+        <div className="ui container">App
+        <div>
+            Select a language
+            <i className="flag us" onClick={() => this.onLanguageChange('english')} />
+            <i className="flag jp" onClick={() => this.onLanguageChange('japanese')}/>
+        </div>
+        {this.state.language}
+        </div>
+     )
+    }
 }
 
 export default App
