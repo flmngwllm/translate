cfeb10cc824b6bf6d28c88d75daa39e8c7146640
William Fleming <chaosmegaman@Williams-MacBook-Pro.local>
2019-07-08 15:29:49 -0400

    added a context folder with context file that holds the context object. In the button component created a context type that links the context object

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
+import LanguageContext from '../context/LanguageContext'
 
 class Button extends Component {
+//created a link to the context object
+    static contextType = LanguageContext;
     render() {
         return (
-            <button className="ui primary">
+            <button className="ui button primary">
                 Submit
             </button>
         );
---
+++ src/context/LanguageContext.js
@@ -0,0 +1,6 @@
+import React from 'react'
+
+//created context object with a default value
+export default React.createContext('english');
+
+
