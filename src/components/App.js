55691e2749975b678d34d437e5c10dc0f95c7dca
William Fleming <chaosmegaman@Williams-MacBook-Pro.local>
2019-07-09 15:29:02 -0400

    Wrapped the language store component around our UserCreate and LanguageSelector components in the App

--- src/components/App.js
+++ src/components/App.js
@@ -1,30 +1,20 @@
 import React,{Component} from 'react'
 import UserCreate from './UserCreate'
-import LanguageContext from '../context/LanguageContext'
+import {LanguageStore } from '../context/LanguageContext'
 import ColorContext from '../context/ColorContext'
 import LanguageSelector from './LanguageSelector'
 
 class App extends Component {
 
-// assigned the language object with a string value to the state
-state = {
-    laguage: 'english'
-}
-
-//setting the state on the laguage object
-onLanguageChange = (language) => {
-    this.setState({language : language})
-}
-
     render(){
      return (
         <div className="ui container">App
-        <LanguageSelector onLanguageChange={this.onLanguageChange} />
+        <LanguageStore>
+        <LanguageSelector />
         <ColorContext.Provider value={"red"}>
-            <LanguageContext.Provider value={this.state.language}>
                 <UserCreate />
-            </LanguageContext.Provider>
         </ColorContext.Provider>
+        </LanguageStore>
         </div>
      )
     }
